73e880c1796c404c488097eeed4275b915a0b482
Mashood P <masoodpookkadan@gmail.com>
2025-12-12 23:15:20 +0400

    video

--- components/HeroVideo.tsx
+++ components/HeroVideo.tsx
@@ -1,46 +1,17 @@
-"use client";
-
-import { useEffect, useRef } from "react";
-
 export default function HeroVideo() {
-    const videoRef = useRef<HTMLVideoElement>(null);
-
-    useEffect(() => {
-        const video = videoRef.current;
-        if (!video) return;
-
-        // Try to play again after hydration
-        const playVideo = () => {
-            video.play().catch(() => { });
-        };
-
-        // try immediately
-        playVideo();
-
-        // try again after slight delay (Safari fix)
-        const timer = setTimeout(playVideo, 200);
-
-        return () => clearTimeout(timer);
-    }, []);
-
     return (
-        <video
-            ref={videoRef}
-            autoPlay
-            loop
-            muted
-            playsInline
-
-            // preload="auto"
-            className="h-full w-full object-cover"
-            webkit-playsinline="true"
-            preload="metadata"
-        >
-            {/* <source src="https://kvrayugaswnnzrlb.public.blob.vercel-storage.com/hero_1080.mp4" type="video/mp4" media="(max-width: 768px)" /> */}
-            {/* <source src="https://kvrayugaswnnzrlb.public.blob.vercel-storage.com/hero.mp4" type="video/mp4" media="(min-width: 769px)" /> */}
-            {/* https://kvrayugaswnnzrlb.public.blob.vercel-storage.com/hero_low.mp4 */}
-            <source src="https://kvrayugaswnnzrlb.public.blob.vercel-storage.com/hero_low.mp4" type="video/mp4" />
-
-        </video>
+        <div className="relative h-full w-full">
+            <video
+                playsInline
+                autoPlay
+                loop
+                muted
+                className="absolute inset-0 h-full w-full object-cover"
+                preload="metadata"
+            >
+                <source src="https://kvrayugaswnnzrlb.public.blob.vercel-storage.com/hero_1080.mp4" type="video/mp4" />
+                <img src="/bottle.png" alt="Hero fallback" />
+            </video>
+        </div>
     );
 }
